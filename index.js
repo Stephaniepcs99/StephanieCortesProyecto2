49aa03e3b22882c2edaf4c73b98e3a92abc26a46
Stephaniepcs99 <400000026@ucamp.utel.edu.mx>
2021-07-30 10:43:54 -0500

    agregamos la funcionalidad de modificar

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const listapedidosUI = document.querySelector('#listped');
 //al dar click en el boton de guardar se realizara lo siguiente
 function onFormSubmit(){
     let formData = LeerDator();
+    if(selecRow==null){
     insertNewRecord(formData);
+    }else{
+        actualizar(formData)
+    }
+    resetForm();
 }
 
 //metodo para leer los datos del formulario
@@ -52,7 +57,51 @@ function insertNewRecord(data){
     celd11=newRow.insertCell(10);
     celd11.innerHTML=data.descripcion;
     celd12=newRow.insertCell(11);
-    celd12.innerHTML=`<a><i class="far fa-edit">editar</i></a> 
-                      <a><i class="fas fa-trash-alt">elim</i></a>`;
+    celd12.innerHTML=`<a onClick="editar(this)"><i class="far fa-edit">editar</i></a> 
+                      <a onClick="eliminar(this)"><i class="fas fa-trash-alt">elim</i></a>`;
+}
+
+//pone en blanco cada uno de los inputs del formulario
+function resetForm(){
+    document.getElementById("c-nom").value="";
+    document.getElementById("c-ape").value="";
+    document.getElementById("c-tel").value="";
+    document.getElementById("c-email").value="";
+    document.getElementById("c-edo").value="";
+    document.getElementById("c-mun").value="";
+    document.getElementById("c-dir").value="";
+    document.getElementById("p-nom").value="";
+    document.getElementById("p-cat").value="";
+    document.getElementById("p-pago").value="";
+    document.getElementById("p-des").value="";
+}
+
+function editar(td){
+    selecRow = td.parentElement.parentElement;
+    document.getElementById("c-nom").value=selecRow.cells[0].innerHTML;
+    document.getElementById("c-ape").value=selecRow.cells[1].innerHTML;
+    document.getElementById("c-tel").value=selecRow.cells[2].innerHTML;
+    document.getElementById("c-email").value=selecRow.cells[3].innerHTML;
+    document.getElementById("c-edo").value=selecRow.cells[4].innerHTML;
+    document.getElementById("c-mun").value=selecRow.cells[5].innerHTML;
+    document.getElementById("c-dir").value=selecRow.cells[6].innerHTML;
+    document.getElementById("p-nom").value=selecRow.cells[7].innerHTML;
+    document.getElementById("p-cat").value=selecRow.cells[8].innerHTML;
+    document.getElementById("p-pago").value=selecRow.cells[9].innerHTML;
+    document.getElementById("p-des").value=selecRow.cells[10].innerHTML;
+}
+
+function actualizar(formData){
+    selecRow.cells[0].innerHTML = formData.nombre;
+    selecRow.cells[1].innerHTML = formData.apellido;
+    selecRow.cells[2].innerHTML = formData.telefono;
+    selecRow.cells[3].innerHTML = formData.email;
+    selecRow.cells[4].innerHTML = formData.estado;
+    selecRow.cells[5].innerHTML = formData.municipio;
+    selecRow.cells[6].innerHTML = formData.direccion;
+    selecRow.cells[7].innerHTML = formData.producto;
+    selecRow.cells[8].innerHTML = formData.categoria;
+    selecRow.cells[9].innerHTML = formData.total;
+    selecRow.cells[10].innerHTML = formData.descripcion;
 }
 
